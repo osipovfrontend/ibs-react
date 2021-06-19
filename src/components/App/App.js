9f6fb45f4010311b4f072c82ee7d9a3cc4f7ccfb
atizay <49369772+atizay@users.noreply.github.com>
2021-06-19 15:29:11 +0500

    Убрал отладочный код

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,9 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import ProductList from '../ProductList/ProductList'
 
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-
 import './App.css'
 
 export default class App extends Component {
-
-  onClickedItem = (id) => {
-    this.setState({
-      clickedProduct: id
-    });
-  };
 
   render() {
     return (
