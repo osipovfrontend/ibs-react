83db570bb95cb25090d83f641ef0299c2c72845b
atizay <49369772+atizay@users.noreply.github.com>
2021-06-19 15:30:45 +0500

    Убрал лишний тег-обертку

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,9 +11,7 @@ export default class App extends Component {
     return (
       <>
         <Header />
-        <main>
-          <ProductList />
-        </main>
+        <ProductList />
       </>
     );
   }
